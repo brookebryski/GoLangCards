08561ee45c7fd15e0ca10ab9e2da9205d6515da0
BrookeBryski <brookeabryski@gmail.com>
2022-05-23 15:26:12 -0700

    logic to save file

--- deck.go
+++ deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // Which is a slice of strings
@@ -32,5 +37,20 @@ func deal(d deck, handSize int) (deck, deck) {
 }
 
 func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
 
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
